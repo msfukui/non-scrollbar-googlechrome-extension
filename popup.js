d8e3cf7ff9a2e662cb3c40d850c15002829e8551
msfukui <msfukui@gmail.com>
2020-08-10 10:26:08 +0900

    Temporary implementation of scrollbar on and off

--- popup.js
+++ popup.js
@@ -7,7 +7,34 @@ chrome.storage.sync.get('scrollbar', function(data) {
 })
 
 scrollbarToggleSwitch.onchange = function(element) {
-  let isScrollbar = element.target.checked
+  const isScrollbar = element.target.checked
+  const noScrollbarStyle =
+      "var scrollbar = getComputedStyle(document.body, \'::-webkit-scrollbar\')\n"
+    + "var css = \'::-webkit-scrollbar { display: none; }\'\n"
+    + "var style = document.createElement(\'style\')\n"
+    + "if (scrollbar.getPropertyValue(\'display\') !== \'none\') {\n"
+    + "  style.appendChild(document.createTextNode(css))\n"
+    + "  document.getElementsByTagName(\'head\')[0].appendChild(style)\n"
+    + "}\n"
+  const scrollbarStyle =
+      "var scrollbar = getComputedStyle(document.body, \'::-webkit-scrollbar\')\n"
+    + "var css = \"::-webkit-scrollbar { display: inline; }\"\n"
+    + "var style = document.createElement(\'style\')\n"
+    + "if (scrollbar.getPropertyValue(\'display\') === \'none\') {\n"
+    + "  style.appendChild(document.createTextNode(css))\n"
+    + "  document.getElementsByTagName(\'head\')[0].appendChild(style)\n"
+    + "}\n"
+  let setStyle = ''
+  let message = ''
+
+  if (isScrollbar) {
+    setStyle = noScrollbarStyle
+    message = 'on'
+  } else {
+    setStyle = scrollbarStyle
+    message = 'off'
+  }
+
   chrome.tabs.query(
     {
       active: true,
@@ -17,18 +44,12 @@ scrollbarToggleSwitch.onchange = function(element) {
       chrome.tabs.executeScript(
         tabs[0].id,
         {
-          code: ''
+          code: setStyle
         }
       )
     }
   )
-  if (isScrollbar) {
-    chrome.storage.sync.set({scrollbar: true}, function() {
-      console.log('The scroll bar is turning on.')
-    })
-  } else {
-    chrome.storage.sync.set({scrollbar: false}, function() {
-      console.log('The scroll bar is turning off.')
-    })
-  }
+  chrome.storage.sync.set({scrollbar: isScrollbar}, function() {
+    console.log('The scroll bar is turning ' + message + '.')
+  })
 }
